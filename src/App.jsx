84d3c211210a88579466ec80f43a108b2382bfad
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-06-09 03:40:22 +0800

    Initialize commit start Login and Chatbox

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+import ChatRoom from "./pages/ChatRoom"
+import Login from "./pages/Login"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      {/* <Login/> */}
+      <ChatRoom/>
+    </div>
+  )
+}
+
+export default App

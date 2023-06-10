c9f17f65dc1e71051526ec0f7ae29b8b0aa064c4
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-06-10 18:23:51 +0800

    import ui bug found

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 import daisyui from 'daisyui';
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",

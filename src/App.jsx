111a7a01019a95371d5c83ae6c9892bf0caac394
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-08-25 15:01:02 +0800

    Project Team and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login";
 import PrivateRoute from "./routes/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
 import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
+import Team from "./pages/Team";
 
 function App() {
   return (
@@ -19,6 +21,22 @@ function App() {
             </PrivateRoute>
           }
         />
+        <Route
+          path="/project"
+          element={
+            <PrivateRoute>
+              <Project />
+            </PrivateRoute>
+          }
+          />
+          <Route 
+            path="/team"
+            element={
+              <PrivateRoute>
+                <Team />
+              </PrivateRoute>
+            }
+          />
       </Routes>
     </AuthProvider>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { UserAuth } from "../context/AuthContext";
 import Dashboard from "../pages/Dashboard";
 import ChatBox from "./ChatBox";
 import SendMessage from "./SendMessage";
+import Project from "../pages/Project";
+import Team from "../pages/Team";
 
 const Navbar = () => {
   const { currentUser, logout } = UserAuth();
@@ -18,6 +20,14 @@ const Navbar = () => {
 
   const navigateDashboard = () => {
     navigate("/dashboard");
+  }
+
+  const navigateProject = () => {
+    navigate("/project");
+  }
+
+  const navigateTeam = () => {
+    navigate("/team");
   }
 
   return (
@@ -51,11 +61,14 @@ const Navbar = () => {
                 </button>
               </li>
               <li>
-                <a>Project</a>
+                <button onClick={navigateProject}>
+                  Project
+                  </button>
               </li>
               <li>
-                <a>Team</a>
-              </li>
+              <button onClick={navigateTeam}>
+                  Team
+                  </button>              </li>
               {/* <li>
                 <button onClick={handleLogout}>Logout</button>
               </li> */}

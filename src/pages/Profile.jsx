4e3c8328aa905abfa311472bdf70b48718857339
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-01 01:39:41 +0800

    Update Profile.jsx
    Resize space, More work needed later

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,11 +5,11 @@ import UserProfile from "../components/UserProfile";
 const Profile = () => {
   return (
     <div className="flex w-full mb-2 px-2">
-      <div className="flex grid h-full w-2/5 pl-2 card place-items-center">
+      <div className="flex grid h-full w-3/5 pl-2 card place-items-center">
         {<TextEditor /> }
       </div>
       <div className="divider px-2"></div>
-      <div className="grid h-screen w-3/5 card bg-base-300 rounded-b-lg place-items-center">
+      <div className="grid h-screen w-2/5 card bg-base-300 rounded-b-lg place-items-center">
         <div className="mx-2">
           <UserProfile />
         </div>

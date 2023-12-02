c305b3c8bcec8bbedac56021953ed344ed1a005d
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-02 23:55:24 +0800

    Remove unnecessary stuff

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,8 @@ import UserProfile from "../components/UserProfile";
 
 const Profile = () => {
   return (
-    <div className="flex w-full mb-2 px-2">
-      <div className="flex grid h-full w-3/5 pl-2 card place-items-center">
-        {<TextEditor /> }
-      </div>
-      <div className="divider px-2"></div>
-      <div className="grid h-screen w-2/5 card bg-base-300 rounded-b-lg place-items-center">
+    <div className="w-full mb-2 px-2">
+      <div className="grid h-screen card bg-base-300 rounded-b-lg place-items-center">
         <div className="mx-2">
           <UserProfile />
         </div>
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -6,7 +6,6 @@ const Team = () => {
   return (
     <div className="flex w-full mb-2 px-2">
       <div className="flex grid h-full w-4/5 pl-2 card place-items-center">
-        <TextEditor />
         <h2>HelloTeam</h2>
       </div>
       <div className="divider px-2"></div>

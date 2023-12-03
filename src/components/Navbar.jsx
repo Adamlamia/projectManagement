fc12771a9fe992699d8e332e899898e036e608c5
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-04 01:54:41 +0800

    Update Navbar.jsx
    Figuring out how to put 2 drawer without rewriting the second drawer content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import ChatBox from "./ChatBox";
 import SendMessage from "./SendMessage";
+import Material from "./Material";
 
 const Navbar = () => {
   const { currentUser, logout } = UserAuth();
@@ -17,19 +18,19 @@ const Navbar = () => {
 
   const navigateDashboard = () => {
     navigate("/dashboard");
-  }
+  };
 
   const navigateProject = () => {
     navigate("/project");
-  }
+  };
 
   const navigateTeam = () => {
     navigate("/team");
-  }
+  };
 
   const navigateProfile = () => {
-    navigate("/profile")
-  }
+    navigate("/profile");
+  };
 
   return (
     <div className="navbar bg-base-100">
@@ -57,19 +58,13 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content absolute z-[1] mt-3 p-2 shadow-2xl bg-base-200 rounded-box w-52"
             >
               <li>
-                <button onClick={navigateDashboard}>
-                  Dashboard
-                </button>
+                <button onClick={navigateDashboard}>Dashboard</button>
               </li>
               <li>
-                <button onClick={navigateProject}>
-                  Project
-                </button>
+                <button onClick={navigateProject}>Project</button>
               </li>
               <li>
-                <button onClick={navigateTeam}>
-                  Team
-                </button>
+                <button onClick={navigateTeam}>Team</button>
               </li>
               <li>
                 <button onClick={navigateProfile}>Profile</button>
@@ -79,31 +74,60 @@ const Navbar = () => {
         </div>
       ) : null}
       <div className="navbar flex justify-center">
-        <button onClick={navigateDashboard} className="btn btn-ghost normal-case text-xl">
+        <button
+          onClick={navigateDashboard}
+          className="btn btn-ghost normal-case text-xl"
+        >
           Project Management
         </button>
       </div>
       {currentUser ? (
         <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator z-0">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
+          {/*<button className="px-2">
+            <div className="drawer drawer-end">
+              <input
+                id="my-drawer-4"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content">
+                <label
+                  htmlFor="my-drawer-4"
+                  className="drawer-button btn btn-ghost btn-circle"
+                >
+                  <div className="indicator z-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                      />
+                    </svg>
+                    <span className="badge badge-xs badge-primary indicator-item"></span>
+                  </div>
+                </label>
+              </div>
+              <div className="drawer-side">
+                <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="menu absolute z-[1] p-4 w-80 h-full bg-base-200 text-base-content">
+                  <div className="absolute inset-x bottom-2">
+                    <Material />
+                  </div>
+                </ul>
+              </div>
             </div>
-          </button>
+          </button> */}
           <button className="px-2">
             <div className="drawer drawer-end">
               <input
@@ -120,8 +144,12 @@ const Navbar = () => {
                 </label>
               </div>
               <div className="drawer-side">
-                <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu absolute z-[1] p-4 w-80 h-full bg-base-200 text-base-content">
+                <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="menu absolute p-4 w-80 h-full bg-base-200 text-base-content">
                   <div className="absolute inset-x bottom-2">
                     <ChatBox />
                     <SendMessage />

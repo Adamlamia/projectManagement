e152bc279603cb3d3c22a067c57dde40e1dbb6ad
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-05 04:59:36 +0800

    Update TextEditor.jsx

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,54 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { BlockNoteView, useBlockNote } from "@blocknote/react";
-import { collection, doc, setDoc } from "@firebase/firestore";
-import { db } from "../firebase";
+import { doc, setDoc, getDoc } from "@firebase/firestore";
 import "@blocknote/core/style.css";
+import { db } from "../firebase"; // Assuming you have a file exporting your Firebase instance
 
 const TextEditor = ({ projectId }) => {
   const editor = useBlockNote({});
   const [blocks, setBlocks] = useState([]);
 
   useEffect(() => {
-    // Listen for content changes in the editor
-    editor.onEditorContentChange(() => {
-      // Update the state with the top-level blocks
-      setBlocks(editor.topLevelBlocks);
+    // Fetch content from Firebase when the component mounts
+    const fetchContent = async () => {
+      try {
+        const projectDoc = await getDoc(doc(db, "projects", projectId));
+        const projectData = projectDoc.data();
+        if (projectData && projectData.editorContent) {
+          // Set the editor content if it exists in the database
+          editor.setContent(projectData.editorContent);
+        }
+      } catch (error) {
+        console.error("Error fetching content:", error);
+      }
+    };
 
-      // Store the content in Firebase
-      storeContentInFirebase(editor.topLevelBlocks, projectId);
-    });
-
-    // Fetch and display the stored content on component mount
-    fetchAndDisplayStoredContent(projectId);
+    fetchContent();
   }, [editor, projectId]);
 
-  const storeContentInFirebase = async (content, projectId) => {
-    try {
-      const projectDocRef = doc(db, "projects", projectId);
-      await setDoc(projectDocRef, { content }, { merge: true });
-    } catch (error) {
-      console.error("Error storing content in Firebase:", error);
-    }
-  };
+  const handleSave = async () => {
+  // Fetch the existing project data from Firebase
+  try {
+    const projectDoc = await getDoc(doc(db, "projects", projectId));
+    const existingProjectData = projectDoc.data();
 
-  const fetchAndDisplayStoredContent = async (projectId) => {
-    try {
-      const projectDoc = await doc(db, "projects", projectId);
-      const projectData = (await projectDoc.get()).data();
+    // Get the current content of the editor
+    const currentContent = editor.topLevelBlocks;
 
-      if (projectData && projectData.content) {
-        // Set the editor's content to the stored content
-        editor.setContent(projectData.content);
-      }
-    } catch (error) {
-      console.error("Error fetching stored content:", error);
-    }
-  };
+    // Update or add the content to Firebase, preserving existing fields
+    await setDoc(doc(db, "projects", projectId), {
+      ...existingProjectData,
+      editorContent: currentContent,
+    });
+
+    console.log("Content saved successfully!");
+  } catch (error) {
+    console.error("Error saving content:", error);
+  }
+};
+
 
   return (
     <div>
       {/* BlockNote editor view */}
       <BlockNoteView editor={editor} />
+
+      {/* Save button */}
+      <button onClick={handleSave}>Save</button>
 
       {/* Display JSON representation of blocks */}
       <pre>{JSON.stringify(blocks, null, 2)}</pre>

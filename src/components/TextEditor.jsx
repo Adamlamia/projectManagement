057486333015b4644e6eaea07fdd0128d3da3a01
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-05 04:30:15 +0800

    Update TextEditor.jsx
    Implements Block to store data into firebase

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,26 +1,58 @@
-import React from "react";
-import {
-  BlockNoteView,
-  defaultReactSlashMenuItems,
-  useBlockNote,
-} from "@blocknote/react";
+import React, { useEffect, useState } from "react";
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import { collection, doc, setDoc } from "@firebase/firestore";
+import { db } from "../firebase";
 import "@blocknote/core/style.css";
 
-const TextEditor = () => {
-  const newSlashMenuItems = defaultReactSlashMenuItems;
+const TextEditor = ({ projectId }) => {
+  const editor = useBlockNote({});
+  const [blocks, setBlocks] = useState([]);
 
-  const editor = useBlockNote({ 
-    slashMenuItems: newSlashMenuItems,
-    
-      editorDOMAttributes: {
-        class: "textEditorStyle",
-      },
-    
+  useEffect(() => {
+    // Listen for content changes in the editor
+    editor.onEditorContentChange(() => {
+      // Update the state with the top-level blocks
+      setBlocks(editor.topLevelBlocks);
+
+      // Store the content in Firebase
+      storeContentInFirebase(editor.topLevelBlocks, projectId);
     });
+
+    // Fetch and display the stored content on component mount
+    fetchAndDisplayStoredContent(projectId);
+  }, [editor, projectId]);
+
+  const storeContentInFirebase = async (content, projectId) => {
+    try {
+      const projectDocRef = doc(db, "projects", projectId);
+      await setDoc(projectDocRef, { content }, { merge: true });
+    } catch (error) {
+      console.error("Error storing content in Firebase:", error);
+    }
+  };
+
+  const fetchAndDisplayStoredContent = async (projectId) => {
+    try {
+      const projectDoc = await doc(db, "projects", projectId);
+      const projectData = (await projectDoc.get()).data();
+
+      if (projectData && projectData.content) {
+        // Set the editor's content to the stored content
+        editor.setContent(projectData.content);
+      }
+    } catch (error) {
+      console.error("Error fetching stored content:", error);
+    }
+  };
+
   return (
-      <div>
-        <BlockNoteView editor={editor} />
-      </div>
+    <div>
+      {/* BlockNote editor view */}
+      <BlockNoteView editor={editor} />
+
+      {/* Display JSON representation of blocks */}
+      <pre>{JSON.stringify(blocks, null, 2)}</pre>
+    </div>
   );
 };
 

fc9e8d85c39664c64e4b05dd47ad0dcc1559eb5b
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2024-01-14 19:26:46 +0800

    Update NewProject.jsx
    Fetching Team List
    Save inside projects collection

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
-import { collection, addDoc } from "@firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { collection, addDoc, getDocs } from "@firebase/firestore";
 import { db } from "../firebase";
 
 const NewProject = () => {
   const [projectName, setProjectName] = useState("");
   const [projectDescription, setProjectDescription] = useState("");
+  const [selectedTeam, setSelectedTeam] = useState(""); // Use string for selectedTeam
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      try {
+        const teamsSnapshot = await getDocs(collection(db, "teams"));
+        const teamsData = teamsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setTeams(teamsData);
+      } catch (error) {
+        console.error("Error fetching teams:", error);
+      }
+    };
+
+    fetchTeams();
+  }, []);
 
   const handleCreateProject = async () => {
     try {
@@ -13,16 +32,22 @@ const NewProject = () => {
         return;
       }
 
+      if (!selectedTeam) {
+        alert("Select a team for the project!");
+        return;
+      }
+
       const projectData = {
         name: projectName,
         description: projectDescription,
+        teamName: selectedTeam, // Save teamName instead of team ID
       };
 
       await addDoc(collection(db, "projects"), projectData);
 
-      // Optionally, you can reset the input fields or close the popup after creating the project
       setProjectName("");
       setProjectDescription("");
+      setSelectedTeam("");
     } catch (error) {
       console.error("Error creating project:", error);
     }
@@ -47,6 +72,24 @@ const NewProject = () => {
             value={projectDescription}
             onChange={(e) => setProjectDescription(e.target.value)}
           />
+        </label>
+        <br />
+        <label>
+          Select Team:
+          <select
+            className="select w-full max-w-xs"
+            value={selectedTeam}
+            onChange={(e) => setSelectedTeam(e.target.value)}
+          >
+            <option disabled value="">
+              Pick a team
+            </option>
+            {teams.map((team) => (
+              <option key={team.id} value={team.teamName}>
+                {team.teamName}
+              </option>
+            ))}
+          </select>
         </label>
         <br />
         <button type="button" onClick={handleCreateProject}>
